1dbbda811e7ec91aa14d5845b0c8c3033e6fd223
zhangenhai <271587754@qq.com>
2021-01-26 20:00:00 +0800

    CloudMusic

---
+++ miniprogram/pages/Music/Music.js
@@ -0,0 +1,134 @@
+// pages/Music/Music.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+   imgUrls:[
+     {
+       url:'http://p1.music.126.net/zUv2mRobckK7Tdn2bp9iSA==/109951165664840470.jpg?imageView&quality=89'
+     },
+    {
+      url:'http://p1.music.126.net/C9I9GxpvRX7nCZyXNBeqOw==/109951165664694558.jpg?imageView&quality=89'
+    },
+    {
+      url:'http://p1.music.126.net/q5rKcBx9Y0V37DsUSaQKXg==/109951165664695730.jpg?imageView&quality=89'
+    },
+    {
+      url:'http://p1.music.126.net/WOoIZuva_umxxzYOvWINLA==/109951165664707565.jpg?imageView&quality=89'
+    },
+    {
+      url:'http://p1.music.126.net/pOXTFta-mhTpZOGhBBWvhQ==/109951165664682857.jpg?imageView&quality=89'
+    },
+    {
+      url:'http://p1.music.126.net/UdSM2BmqY_h_t9HAOzb5dQ==/109951165664710664.jpg?imageView&quality=89'
+    },
+    {
+      url:'http://p1.music.126.net/Z90NF2dHuBYrV6x-U9jJJQ==/109951165664719544.jpg?imageView&quality=89'
+    },
+    {
+      url:'http://p1.music.126.net/vAjwukVm-H0LOqzy4FTJXA==/109951165664851877.jpg?imageView&quality=89'
+    },
+    {
+      url:'http://p1.music.126.net/9Ayx-EeCnuLRWKTcIhGB6g==/109951165664742856.jpg?imageView&quality=89'
+    }
+   ],
+   playlist:[
+     {
+       "id":"1001",
+      " playCount":1.4641238e+06,
+      "name":"你一定要在自己热爱的世界里闪闪发亮啊",
+      "picUrl":"http://p3.music.126.net/uesfHcJmZ23S3er_1mpeaw==/109951165621856219.jpg?param=140y14"
+    },
+    {
+      "id":"1002",
+      " playCount":622822.6,
+      "name":"我再也不会对谁满怀期待了",
+      "picUrl":"http://p3.music.126.net/-Nb42Jwn9dkZR2AtvyTBrQ==/109951165571613240.jpg?param=140y140"
+    },
+    {
+      "id":"1003",
+      " playCount":65872.6,
+      "name":"所有好运都会和你撞个满怀",
+      "picUrl":"http://p3.music.126.net/l_c3SARUoRRhNOLkhZJByA==/109951165077363897.jpg?param=140y140"
+    },
+
+    {
+      "id":"1004",
+      " playCount":7.1546155e+06,
+      "name":"AOMG女孩的晚安",
+      "picUrl":"http://p3.music.126.net/IdZ8qbwz9agLI5-HZDPeSQ==/109951163912181325.jpg?param=140y140"
+    },
+    {
+      "id":"1005",
+      " playCount":1.01546155e+08,
+      "name":"Show Me The Money 777",
+      "picUrl":"http://p3.music.126.net/pTV8SA6fxIE-NIZFJTFtxQ==/109951163395122398.jpg?param=140y140"
+    },
+    {
+      "id":"1006",
+      " playCount":1.015155e+06,
+      "name":"超好听的韩语歌单！",
+      "picUrl":"http://p4.music.126.net/g4aahimgeyfnHeBrjL1QBw==/109951165646209352.jpg?param=140y140"
+
+    }
+   ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
